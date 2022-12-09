e94e362214fcddcf9bbec8e0ebc3ea1d06744f31
beckerd <daniel.becker@gdv-dl.de>
2022-12-09 14:24:01 +0100

    day3

---
+++ 2022/Day3 - Rucksack Reorganization/solution.py
@@ -0,0 +1,36 @@
+# Settings
+inputPath = r'C:/Github-Projects/AdventOfCode/2022/Day3 - Rucksack Reorganization/input.txt'
+
+foundLetters = []
+points = 0
+
+def compare(str1, str2):
+    i = 0
+    while i < len(str1):
+        if str1[i] in str2:
+            foundLetters.append(str1[i])
+            break
+        i += 1
+
+with open(inputPath, 'r', encoding="utf-8") as file:
+    lines = file.readlines()
+
+    for line in lines:
+        length = len(line)
+        firstHalf = line[0:length//2]
+        SecondHalf = line[length//2:]
+        print("String:", firstHalf, "|", SecondHalf)
+        compare(firstHalf, SecondHalf)
+
+    print("Found:",foundLetters)
+    for char in foundLetters:
+        if char.islower():
+            idOfLetter = ord(char) - 96
+            points += idOfLetter
+            print("Char", char, ":", idOfLetter, "points")
+        if char.isupper():
+            idOfLetter = ord(char.lower()) - 96 +26
+            points += idOfLetter
+            print("Char", char, ":", idOfLetter, "points")
+    print ("Total points:",points)
+
---
+++ 2022/Day3 - Rucksack Reorganization/solution_part2.py
@@ -0,0 +1,53 @@
+# Settings
+inputPath = r'C:/Github-Projects/AdventOfCode/2022/Day3 - Rucksack Reorganization/input.txt'
+
+foundLetters = []
+
+def compare(str1, str2, str3):
+    foundLetters = []
+    i = 0
+    points = 0
+    print("String1", str1, "String2", str2, "String3", str3)
+    while i < len(str1):
+        if str1[i] in str2 and str1[i] not in foundLetters:
+            if str1[i] in str3:
+                print("Found letter: ", str1[i])
+                foundLetters.append(str1[i])
+                points += getPointsForLetter(str1[i])
+        i += 1
+    print(foundLetters)
+    return points
+
+def getPointsForLetter(char):
+    points = 0
+    idOfLetter = 0
+    
+    if char.islower():
+        idOfLetter = ord(char) - 96
+    
+    if char.isupper():
+        idOfLetter = ord(char.lower()) - 96 +26
+    
+    points = points + idOfLetter
+    print("Char", char, ":", idOfLetter, "points")
+    
+    return points
+
+with open(inputPath, 'r', encoding="utf-8") as file:
+    lines = file.readlines()
+
+    itemsInARucksack = 3
+    startAtLine = 0
+    endAtLine = itemsInARucksack
+
+    points = 0
+
+    i = 0
+    while i < len(lines)/itemsInARucksack:
+        arr = lines[startAtLine:endAtLine]
+        points += compare(arr[0].strip(), arr[1].strip(), arr[2].strip())
+        i += 1
+        startAtLine += itemsInARucksack
+        endAtLine += itemsInARucksack
+    print ("Total points:",points)
+
