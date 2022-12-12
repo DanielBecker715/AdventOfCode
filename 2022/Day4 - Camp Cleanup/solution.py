e5fbe73d6020488f89dafd30a390697aeaf354e5
beckerd <daniel.becker@gdv-dl.de>
2022-12-12 12:36:41 +0100

    day4

---
+++ 2022/Day4 - Camp Cleanup/solution.py
@@ -0,0 +1,36 @@
+# Settings
+inputPath = r'C:/Github-Projects/AdventOfCode/2022/Day4 - Camp Cleanup/input.txt'
+
+def comparePairs(firstPairRange: list[int], SecondPairRange: list[int]):
+    maxFirstPair = firstPairRange[firstPairRange.index(max(firstPairRange))]
+    minFirstPair = firstPairRange[firstPairRange.index(min(firstPairRange))]
+    
+    maxSecondPair = SecondPairRange[SecondPairRange.index(max(SecondPairRange))]
+    minSecondPair = SecondPairRange[SecondPairRange.index(min(SecondPairRange))]
+
+    if (maxFirstPair >= maxSecondPair and minFirstPair <= minSecondPair):
+        print ("includes")
+        return 1
+    if (maxSecondPair >= maxFirstPair and minSecondPair <= minFirstPair):
+        print ("includes")
+        return 1
+    return 0
+
+with open(inputPath, 'r', encoding="utf-8") as file:
+    lines = file.readlines()
+    counter = 0
+    for line in lines:
+        line.strip()
+        print("--- Pairs:",line, "---")
+
+        sectionRange = line.strip().split(",")
+        firstPairRange = sectionRange[0].strip().split("-")
+        SecondPairRange = sectionRange[1].strip().split("-")
+
+        #Convert to int
+        firstPairRange = list(map(int, firstPairRange))
+        SecondPairRange = list(map(int, SecondPairRange))
+
+        counter = counter + comparePairs(firstPairRange, SecondPairRange)
+        print("--- Line-End ---")
+    print("Counted Pairs:",counter)
---
+++ 2022/Day4 - Camp Cleanup/solution_part2.py
@@ -0,0 +1,42 @@
+# Settings
+inputPath = r'C:/Github-Projects/AdventOfCode/2022/Day4 - Camp Cleanup/input.txt'
+
+def comparePairs(firstPairRange: list[int], SecondPairRange: list[int]):
+    maxFirstPair = firstPairRange[firstPairRange.index(max(firstPairRange))]
+    minFirstPair = firstPairRange[firstPairRange.index(min(firstPairRange))]
+    
+    maxSecondPair = SecondPairRange[SecondPairRange.index(max(SecondPairRange))]
+    minSecondPair = SecondPairRange[SecondPairRange.index(min(SecondPairRange))]
+
+    if(maxFirstPair >= maxSecondPair and minFirstPair <= minSecondPair):
+        print("includes")
+        return 1
+    if(maxFirstPair >= maxSecondPair and minFirstPair <= maxSecondPair):
+        print("includes")
+        return 1
+    if(maxSecondPair >= maxFirstPair and minSecondPair <= minFirstPair):
+        print("includes")
+        return 1
+    if(maxSecondPair >= maxFirstPair and minSecondPair <= maxFirstPair):
+        print("includes")
+        return 1
+
+    return 0
+
+with open(inputPath, 'r', encoding="utf-8") as file:
+    lines = file.readlines()
+    counter = 0
+    for line in lines:
+        print("--- Pairs:", line.rstrip(), "---")
+
+        sectionRange = line.strip().split(",")
+        firstPairRange = sectionRange[0].rstrip().split("-")
+        SecondPairRange = sectionRange[1].rstrip().split("-")
+
+        #Convert to int
+        firstPairRange = list(map(int, firstPairRange))
+        SecondPairRange = list(map(int, SecondPairRange))
+
+        counter = counter + comparePairs(firstPairRange, SecondPairRange)
+        print("--- Line-End ---\n")
+    print("Counted Pairs:",counter)
