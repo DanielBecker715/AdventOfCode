014a7704be89b63b78ae8c46457d72a8dfc29380
beckerd <daniel.becker@gdv-dl.de>
2022-12-13 09:35:05 +0100

    day6

---
+++ 2022/Day6 - Tuning Trouble/solution.py
@@ -0,0 +1,18 @@
+# Settings
+inputPath = r'C:/Github-Projects/AdventOfCode/2022/Day6 - Tuning Trouble/input.txt'
+
+with open(inputPath, 'r', encoding="utf-8") as file:
+    data = file.read()
+
+p1 = False
+p2 = False
+
+for i in range(4, len(data)):
+    part1 = set(data[(i-4):i])
+    part2 = set(data[(i-14):i])
+    if (not p1 and len(part1) == 4):
+        print("Part1: ",part1, i)
+        p1 = True
+    if (not p2 and len(part2) == 14):
+        print("Part2: ",part2, i)
+        p2 = True
